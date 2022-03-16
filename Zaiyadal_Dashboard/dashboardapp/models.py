cb2fd5f72e45637c9a96fcf17fd2536777551b58
nadziroar <nadzirahzaid144@gmail.com>
2022-03-16 15:47:23 +0800

    Update models.py

--- Zaiyadal_Dashboard/dashboardapp/models.py
+++ Zaiyadal_Dashboard/dashboardapp/models.py
@@ -3,7 +3,16 @@ from django.db import models
 # Create your models here.
 #This is class for Parent
 class ParentClass (models.Model):
-    name = models.CharField(max_length= 200)
+    first_name = models.CharField(max_length= 200)
+    gender = [
+        ('M' , 'Male') , 
+        ('F' , 'Female'),
+        ]
+    status = [
+        ('S' , 'Single') , 
+        ('M' , 'Married'),
+        ('W' , 'Widow'),
+        ]
     createddate = models.DateTimeField(auto_now_add=True)
     createdby = models.CharField (max_length= 200 , default= 'Admin')
     updatedate = models.DateTimeField (auto_now_add= True)
