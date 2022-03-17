f37a6cf33f5cada04aaff55f03c828da4e4bd912
nadziroar <nadzirahzaid144@gmail.com>
2022-03-17 09:36:22 +0800

    Update main.py

--- Netflix_Subscriptions/main.py
+++ Netflix_Subscriptions/main.py
@@ -1,14 +1,24 @@
 #This is main file to display the graph of The price of netflix subscription throughout the countries
 
 #Before starting install these : 
+#1. pip3 install pandas
+#2. pip3 install np
 #1. pip install pycountry-convert (to GET COUNTRY_CODES & CONTINENTS)
 #2.  pip3 install geopy - To get longitude & Latitude
-
-
+#3. pip3 install folium -  To create world map
+#
+#To import data from data.csv
+import numpy as np
+import pandas as pd
 # This is function to convert to country codes and  continents
 from pycountry_convert import country_alpha2_to_continent_code, country_name_to_country_alpha2
 #This function to get longtitude and latitude
 from geopy.geocoders import Nominatim
+# Create world map : 
+import folium
+from folium.plugins import MarkerCluster
+
+
 #Function to get continent & country code
 def get_continent(col):
     try: 
@@ -33,3 +43,23 @@ def geolocate (country):
     except : 
         #Return mising value 
         return ('NaN ' , 'NaN')
+
+#To create empty map : 
+world_map = folium.Map(tiles = "cartodbpositron")
+
+marker_cluster = MarkerCluster().add_to(world_map)
+
+#For each coordinate , create circlemarker of user percent
+for i in range(len(df)):
+    lat = df.iloc[i]['Latitude']
+    long = df.iloc[i]['Longtitude']
+    radius = 5
+    popup_text = """Country : {} <br> %of Users : {}"""
+    popup_text = popup_text.format(df.iloc[i]['Country'], df.iloc[i]['User_Percent'])
+    
+    folium.CircleMarker(location = [lat,long] , radius = radius, popup = popup_text , fill= True).add_to(marker_cluster)
+
+#Display the map
+world_map
+    
+
